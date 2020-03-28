43945af23b66aab5c19ae4f1a5a9c09c2e966828
John Zetterström <z@draqir.se>
2020-03-28 20:03:03 +0100

    Homepage fixes

--- src/components/ButtonLink/index.tsx
+++ src/components/ButtonLink/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const StyledLink = styled(Link)`
   background: #d8d8d8;
-  padding: 8px 16px;
+  padding: 0 16px;
+  line-height: 44px;
+  height: 44px;
   border-radius: 100px;
   color: black;
   text-decoration: none;
+  display: inline-block;
+  margin-left: 8px;
+  font-weight: 900;
+  font-style: normal;
+  font-family: Avenir;
+  font-size: 17px;
 `;
 
 type ButtonProps = React.HTMLAttributes<HTMLAnchorElement> & {
